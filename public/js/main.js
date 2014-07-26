d65262f2962ba9a3654d8fb4fff146377986c8a5
Quentin Dufour <quentin@dufour.tk>
2014-07-27 00:37:18 +0200

    Graphical improve

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   $.get( "api/report", function( data ) {
     data.forEach(function(report) {
-      $("#timeline").append('<blockquote class="twitter-tweet"><a href="https://twitter.com/'+report['offensiveUser.userId']+'/statuses/'+report['offensiveUser.statusId']+'">View on Twitter</a></blockquote>');
+      $("#timeline").append('<h3>'+report.reported+' offended people</h3><blockquote class="twitter-tweet"><a href="https://twitter.com/'+report['offensiveUser.userId']+'/statuses/'+report['offensiveUser.statusId']+'">View on Twitter</a></blockquote><hr>');
     });
     twttr.widgets.load();
   });
